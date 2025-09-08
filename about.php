7a10c22dba1b778c4874c2bc7d1467c9899446ec
Thisal Chathnuka <wtchathnuka@students.nsbm.ac.lk>
2025-09-08 21:55:39 +0530

    Update about.php

--- about.php
+++ about.php
@@ -11,8 +11,6 @@
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;700&display=swap" rel="stylesheet" />
   <link href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" rel="stylesheet" />
 
-
-  
   <style>
     :root {
       --brand-bg: #0b1e3f;
@@ -97,8 +95,6 @@
 </head>
 <body>
 
-
-  
 <!-- Navbar -->
 <nav class="navbar navbar-expand-lg navbar-dark fixed-top">
   <a class="navbar-brand text-warning" href="index.php">Vinal Auto</a>
@@ -198,6 +194,7 @@
     </div>
   </div>
 </footer>
+
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
 <script>
