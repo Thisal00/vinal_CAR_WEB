7477d25430d77ec06caafc4f8359847df5d4a2b0
vidath alahakoon <vidathalahakoon2@gmail.com>
2025-09-15 13:32:27 +0530

    Create users.php

---
+++ admin/users.php
@@ -0,0 +1,11 @@
+<nav class="nav-links">
+         
+        <a href="vehicles.php">Vehicles</a>
+        <a href="parts.php">Parts</a>
+        <a href="part_messages.php">Orders</a>
+        <a href="users.php">Users</a>
+        <a href="messages.php">Messages</a>
+        <a href="reviews.php">Reviews</a>
+        <a href="bookings.php" >Test Drives</a>
+        <a href="compare_add.php">Compare</a>
+    </nav>
